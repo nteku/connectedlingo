70175ef92d4c3c8e6409c5d557d40878f8347514
Nteku <n.teku710@gmail.com>
2023-07-14 22:17:06 -0700

    Finished UI Layout for LoginPage

--- app/src/main/java/com/maid/connectedlingo/MainActivity.kt
+++ app/src/main/java/com/maid/connectedlingo/MainActivity.kt
@@ -3,15 +3,19 @@ package com.maid.connectedlingo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Window
+import android.widget.TextView
 
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var registerLink : TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
 
-
+        registerLink = findViewById(R.id.register)
 
 
 
